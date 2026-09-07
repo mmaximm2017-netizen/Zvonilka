2b2074bd448844aecacd6a831e658ec9eedf1942
mmaximm2017-netizen <mmaximm2017@gmail.com>
2026-09-07 16:59:33 +0300

    Bump Zvonilka to 0.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ru.zvonilka.prototype"
         minSdk = 29
         targetSdk = 35
-        versionCode = 17
-        versionName = "0.10.3"
+        versionCode = 18
+        versionName = "0.11.0"
     }
     buildFeatures {
         compose = true
